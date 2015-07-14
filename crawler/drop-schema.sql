6028141c915c36c5106ad0457ad3c1cd5d98eb35
aureliano <aureliano.franca@hotmail.com>
2015-07-14 15:33:08 -0300

    API data schema.

---
+++ crawler/drop-schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE verse;
+DROP TABLE chapter;
+DROP TABLE book;
+DROP TABLE bible;
+
+DROP SEQUENCE bible_seq;
+DROP SEQUENCE book_seq;
+DROP SEQUENCE chapter_seq;
+DROP SEQUENCE verse_seq;
+
+/*
+DELETE FROM verse;
+DELETE FROM chapter;
+DELETE FROM book;
+DELETE FROM bible;
+*/
