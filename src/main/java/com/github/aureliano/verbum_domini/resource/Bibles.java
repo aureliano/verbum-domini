da44f85754ac3554f0b4563b373df0f36244c89a
aureliano <aureliano.franca@hotmail.com>
2015-07-14 16:40:07 -0300

    Create bible resource interface.

---
+++ src/main/java/com/github/aureliano/verbum_domini/resource/Bibles.java
@@ -0,0 +1,94 @@
+package com.github.aureliano.verbum_domini.resource;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import com.github.aureliano.verbum_domini.model.Bible;
+
+@Path("bibles")
+public interface Bibles {
+
+	/**
+	 * 
+	 * @param start
+	 *	 The first page to return
+	 * @param pages
+	 *	 The number of pages to return
+	 */
+	@GET
+	@Produces({
+		"application/json"
+	})
+	Bibles.GetBiblesResponse getBibles(
+		@QueryParam("start")
+		Long start,
+		@QueryParam("pages")
+		Long pages)
+		throws Exception
+	;
+
+	/**
+	 * 
+	 * @param language
+	 *	 
+	 */
+	@GET
+	@Path("{language}")
+	@Produces({
+		"application/json"
+	})
+	Bibles.GetBiblesByLanguageResponse getBiblesByLanguage(
+		@PathParam("language")
+		String language)
+		throws Exception
+	;
+
+	public class GetBiblesByLanguageResponse
+		extends com.github.aureliano.verbum_domini.support.ResponseWrapper
+	{
+
+
+		private GetBiblesByLanguageResponse(Response delegate) {
+			super(delegate);
+		}
+
+		/**
+		 * OK
+		 * 
+		 * @param entity
+		 *	 
+		 */
+		public static Bibles.GetBiblesByLanguageResponse jsonOK(Bible entity) {
+			Response.ResponseBuilder responseBuilder = Response.status(200).header("Content-Type", "application/json");
+			responseBuilder.entity(entity);
+			return new Bibles.GetBiblesByLanguageResponse(responseBuilder.build());
+		}
+
+	}
+
+	public class GetBiblesResponse
+		extends com.github.aureliano.verbum_domini.support.ResponseWrapper
+	{
+
+
+		private GetBiblesResponse(Response delegate) {
+			super(delegate);
+		}
+
+		/**
+		 * OK
+		 * 
+		 * @param entity
+		 *	 
+		 */
+		public static Bibles.GetBiblesResponse jsonOK(com.github.aureliano.verbum_domini.model.Bibles entity) {
+			Response.ResponseBuilder responseBuilder = Response.status(200).header("Content-Type", "application/json");
+			responseBuilder.entity(entity);
+			return new Bibles.GetBiblesResponse(responseBuilder.build());
+		}
+
+	}
+}
