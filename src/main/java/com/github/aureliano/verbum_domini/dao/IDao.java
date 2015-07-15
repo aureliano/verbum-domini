3b25bc081acf6d600d8df726f8165d9dd54b318e
aureliano <aureliano.franca@hotmail.com>
2015-07-15 10:26:25 -0300

    Use pagination object as return type for collection returning methods.

--- src/main/java/com/github/aureliano/verbum_domini/dao/IDao.java
+++ src/main/java/com/github/aureliano/verbum_domini/dao/IDao.java
@@ -1,17 +1,18 @@
 package com.github.aureliano.verbum_domini.dao;
 
 import java.io.Serializable;
-import java.util.List;
 
 import com.github.aureliano.verbum_domini.web.ServiceParams;
 
 public interface IDao<T> {
 
-	public abstract List<Pagination<T>> list();
+	public static final Integer PAGE_ELEMENTS = 50;
+
+	public abstract Pagination<T> list();
 	
-	public abstract List<Pagination<T>> list(T filter);
+	public abstract Pagination<T> list(T filter);
 	
-	public abstract List<Pagination<T>> list(T filter, ServiceParams params);
+	public abstract Pagination<T> list(T filter, ServiceParams params);
 	
 	public abstract T load(Serializable id);
 }
