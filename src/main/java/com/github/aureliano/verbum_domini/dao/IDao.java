6afeca88caaa3b18a400e19a6e4085ed4dc23baa
aureliano <aureliano.franca@hotmail.com>
2015-07-15 10:03:09 -0300

    Prepare data access object interface.

--- src/main/java/com/github/aureliano/verbum_domini/dao/IDao.java
+++ src/main/java/com/github/aureliano/verbum_domini/dao/IDao.java
@@ -3,9 +3,15 @@ package com.github.aureliano.verbum_domini.dao;
 import java.io.Serializable;
 import java.util.List;
 
+import com.github.aureliano.verbum_domini.web.ServiceParams;
+
 public interface IDao<T> {
 
-	public List<T> list();
+	public abstract List<Pagination<T>> list();
 	
-	public T load(Serializable id);
+	public abstract List<Pagination<T>> list(T filter);
+	
+	public abstract List<Pagination<T>> list(T filter, ServiceParams params);
+	
+	public abstract T load(Serializable id);
 }
