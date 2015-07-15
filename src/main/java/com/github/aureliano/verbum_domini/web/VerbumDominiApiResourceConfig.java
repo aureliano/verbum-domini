5b4c707ae9e7f60aa458948eb1b93df38e80b197
aureliano <aureliano.franca@hotmail.com>
2015-07-15 10:51:09 -0300

    Create API resource configuration.

---
+++ src/main/java/com/github/aureliano/verbum_domini/web/VerbumDominiApiResourceConfig.java
@@ -0,0 +1,14 @@
+package com.github.aureliano.verbum_domini.web;
+
+import org.glassfish.jersey.server.ResourceConfig;
+
+import com.github.aureliano.verbum_domini.resource.impl.BiblesResourceImpl;
+
+public class VerbumDominiApiResourceConfig extends ResourceConfig {
+
+	public VerbumDominiApiResourceConfig() {
+		packages("com.github.aureliano.verbum_domini.resource.impl");
+		
+		register(BiblesResourceImpl.class);
+	}
+}
