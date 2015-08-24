4a079b4d1c58126d904715f4435fc7705ba201f1
aureliano <aureliano.franca@hotmail.com>
2015-08-24 15:51:40 -0300

    Create model for entity bean Book.

---
+++ src/main/java/com/github/aureliano/verbum_domini/model/Book.java
@@ -0,0 +1,151 @@
+package com.github.aureliano.verbum_domini.model;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+/**
+ * Books within the Holy Bible
+ * 
+ */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Generated("org.jsonschema2pojo")
+@JsonPropertyOrder({
+    "id",
+    "name",
+    "bibleId"
+})
+public class Book {
+
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("id")
+    private Integer id;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("name")
+    private String name;
+    /**
+     * 
+     * (Required)
+     * 
+     */
+    @JsonProperty("bibleId")
+    private Integer bibleId;
+    @JsonIgnore
+    private Map<String, Object> additionalProperties = new HashMap<String, Object>();
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @return
+     *     The id
+     */
+    @JsonProperty("id")
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @param id
+     *     The id
+     */
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Book withId(Integer id) {
+        this.id = id;
+        return this;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @return
+     *     The name
+     */
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @param name
+     *     The name
+     */
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Book withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @return
+     *     The bibleId
+     */
+    @JsonProperty("bibleId")
+    public Integer getBibleId() {
+        return bibleId;
+    }
+
+    /**
+     * 
+     * (Required)
+     * 
+     * @param bibleId
+     *     The bibleId
+     */
+    @JsonProperty("bibleId")
+    public void setBibleId(Integer bibleId) {
+        this.bibleId = bibleId;
+    }
+
+    public Book withBibleId(Integer bibleId) {
+        this.bibleId = bibleId;
+        return this;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalProperties() {
+        return this.additionalProperties;
+    }
+
+    @JsonAnySetter
+    public void setAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+    }
+
+    public Book withAdditionalProperty(String name, Object value) {
+        this.additionalProperties.put(name, value);
+        return this;
+    }
+}
