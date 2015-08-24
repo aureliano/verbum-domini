b1c2e5a94c35a30d114541d746bbcdb33e931872
aureliano <aureliano.franca@hotmail.com>
2015-08-24 17:48:42 -0300

    src/main/java/com/github/aureliano/verbum_domini/dao/BookDao.java

---
+++ src/main/java/com/github/aureliano/verbum_domini/service/BooksService.java
@@ -0,0 +1,14 @@
+package com.github.aureliano.verbum_domini.service;
+
+import com.github.aureliano.verbum_domini.model.Books;
+
+public final class BooksService {
+
+	public BooksService() {
+		super();
+	}
+	
+	public static Books fetchBooksByBible(String language, Long start, Long pages) {
+		return null;
+	}
+}
