2d7258809acf15d6c351bf4a4f4e37170e547826
aureliano <aureliano.franca@hotmail.com>
2015-08-24 18:09:47 -0300

    Create project custom exception.

---
+++ src/main/java/com/github/aureliano/verbum_domini/exception/VerbumDominiException.java
@@ -0,0 +1,22 @@
+package com.github.aureliano.verbum_domini.exception;
+
+public class VerbumDominiException extends RuntimeException {
+
+	private static final long serialVersionUID = -301093296630058559L;
+
+	public VerbumDominiException() {
+		super();
+	}
+	
+	public VerbumDominiException(String message) {
+		super(message);
+	}
+	
+	public VerbumDominiException(Throwable throwable) {
+		super(throwable);
+	}
+	
+	public VerbumDominiException(String message, Throwable throwable) {
+		super(message, throwable);
+	}
+}
