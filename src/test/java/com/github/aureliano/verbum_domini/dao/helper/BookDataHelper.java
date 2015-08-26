dc8f8a918142da10bfafc72d0432306539e73239
aureliano <aureliano.franca@hotmail.com>
2015-08-25 23:05:27 -0300

    Create book data and test unit.

--- src/test/java/com/github/aureliano/verbum_domini/dao/helper/BookDataHelper.java
+++ src/test/java/com/github/aureliano/verbum_domini/dao/helper/BookDataHelper.java
@@ -1,7 +1,11 @@
 package com.github.aureliano.verbum_domini.dao.helper;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
 import com.github.aureliano.verbum_domini.bean.BibleBean;
 import com.github.aureliano.verbum_domini.bean.BookBean;
+import com.github.aureliano.verbum_domini.orm.PersistenceManager;
 
 public final class BookDataHelper {
 
@@ -10,14 +14,33 @@ public final class BookDataHelper {
 	}
 	
 	public static void createBooks() {
+		String[] names = new String[] {
+			"Genesis", "Exodus", "Leviticus", "Numeri", "Deuteronomii"
+		};
 		
+		Session session = PersistenceManager.instance().openSession();
+		int[] ids = new int[] { 1, 2 };
+		int bookId = 0;
+		
+		for (int id : ids) {
+			BibleBean bible = (BibleBean) session.load(BibleBean.class, id);
+			
+			Transaction transaction = session.beginTransaction();
+			for (String name : names) {
+				BookBean book = prepareBook(++bookId, name, bible);
+				session.saveOrUpdate(book);
+			}
+			
+			transaction.commit();
+		}
 	}
 	
-	private static BookBean createBook(Integer id, BibleBean bible) {
+	private static BookBean prepareBook(Integer id, String name, BibleBean bible) {
 		BookBean book = new BookBean();
 		
 		book.setId(id);
-		book.setName(name)
+		book.setName(name);
+		book.setBible(bible);
 		
 		return book;
 	}
