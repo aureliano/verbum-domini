ae2acada35f6657eee2f212f2ec43dd2568216ef
aureliano <aureliano.franca@hotmail.com>
2015-08-25 23:04:08 -0300

    Create singleton class for managing DAO units data preparation.

---
+++ src/test/java/com/github/aureliano/verbum_domini/dao/helper/DataHelper.java
@@ -0,0 +1,38 @@
+package com.github.aureliano.verbum_domini.dao.helper;
+
+import com.github.aureliano.verbum_domini.orm.PersistenceManager;
+
+public final class DataHelper {
+
+	private static DataHelper instance;
+	
+	private boolean dataHelpersInitialized;
+	
+	private DataHelper() {
+		this.dataHelpersInitialized = false;
+	}
+	
+	public static DataHelper instance() {
+		if (instance == null) {
+			instance = new DataHelper();
+		}
+		
+		return instance;
+	}
+	
+	public void initializeDataHelpers() {
+		if (this.dataHelpersInitialized) {
+			return;
+		}
+		
+		PersistenceManager.instance().startUp();
+		BibleDataHelper.createBibles();
+		BookDataHelper.createBooks();
+		
+		this.dataHelpersInitialized = true;
+	}
+	
+	public boolean isDataHelpersInitialized() {
+		return this.dataHelpersInitialized;
+	}
+}
