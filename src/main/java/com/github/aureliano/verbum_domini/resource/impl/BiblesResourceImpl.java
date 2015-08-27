f2d3d21ea872c6fac6c91b977694c22b242feddf
aureliano <aureliano.franca@hotmail.com>
2015-08-27 17:38:56 -0300

    Remove unused imports.

--- src/main/java/com/github/aureliano/verbum_domini/resource/impl/BiblesResourceImpl.java
+++ src/main/java/com/github/aureliano/verbum_domini/resource/impl/BiblesResourceImpl.java
@@ -1,11 +1,5 @@
 package com.github.aureliano.verbum_domini.resource.impl;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-
 import com.github.aureliano.verbum_domini.resource.Bibles;
 import com.github.aureliano.verbum_domini.service.BiblesService;
 import com.github.aureliano.verbum_domini.service.BooksService;
