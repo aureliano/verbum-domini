8c9e5b52bda7bf0a0b9f45f823775b86212a06cd
aureliano <aureliano.franca@hotmail.com>
2015-08-31 10:17:30 -0300

    Prepare mongodb collection data filling.

--- crawler/mongo_data_fill_header.rb
+++ crawler/mongo_data_fill_header.rb
@@ -2,15 +2,30 @@ require 'mongo'
 include Mongo
 
 database = 'verbum_domini'
-@bible_id = @book_id = @chapter_id = @verse_id = @annotation_id = 0
 url = ENV['MONGOLAB_URI'] ||= "mongodb://#{Connection::DEFAULT_HOST}:#{Connection::DEFAULT_PORT}"
 
-client = Connection.from_uri(ENV['MONGOLAB_URI']).db(database)
+@client = Connection.from_uri(ENV['MONGOLAB_URI']).db(database)
 
 require 'json'
 
-@coll_bible = client.create_collection 'bible'
-@coll_book = client.create_collection 'book'
-@coll_chapter = client.create_collection 'chapter'
-@coll_verse = client.create_collection 'verse'
-@coll_annotation = client.create_collection 'annotation'
+def load_collection(name)
+  coll = @client.collection name
+  (coll.nil?) ? @client.create_collection(name) : coll
+end
+
+@coll_bible = load_collection 'bible'
+@coll_book = load_collection 'book'
+@coll_chapter = load_collection 'chapter'
+@coll_verse = load_collection 'verse'
+@coll_annotation = load_collection 'annotation'
+
+def fetch_last_id(collection)
+  data = collection.find().sort(:_id => -1).limit(1).first
+  (data.nil?) ? 0 : data['_id']
+end
+
+@bible_id = fetch_last_id @coll_bible
+@book_id = fetch_last_id @coll_book
+@chapter_id = fetch_last_id @coll_chapter
+@verse_id = fetch_last_id @coll_verse
+@annotation_id = fetch_last_id @coll_annotation
