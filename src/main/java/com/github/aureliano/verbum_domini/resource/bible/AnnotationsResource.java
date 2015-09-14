1a1187eb9ced04b625bd8a74b910590a592f0876
aureliano <aureliano.franca@hotmail.com>
2015-09-13 22:38:40 -0300

    Fetch annotations service.

---
+++ src/main/java/com/github/aureliano/verbum_domini/resource/bible/AnnotationsResource.java
@@ -0,0 +1,46 @@
+package com.github.aureliano.verbum_domini.resource.bible;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.github.aureliano.verbum_domini.model.Annotation;
+import com.github.aureliano.verbum_domini.model.Annotations;
+import com.github.aureliano.verbum_domini.service.AnnotationsService;
+
+@Path("annotations")
+public class AnnotationsResource {
+
+	public AnnotationsResource() {
+		super();
+	}
+	
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response getAnnotations(
+			@QueryParam("start") Long start,
+			@QueryParam("pages") Long pages) {
+		
+		Annotations annotations = AnnotationsService.fetchAll(start, pages);
+		return Response.status(200).entity(annotations).build();
+	}
+
+	@Path("{annotationId}")
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response getAnnotationById(
+			@PathParam("annotationId") String annotationId) {
+		
+		Annotation annotation = AnnotationsService.fetchAnnotationById(annotationId);
+		
+		if (annotation == null) {
+			return Response.status(404).build();
+		}
+		
+		return Response.status(200).entity(annotation).build();
+	}
+}
--- src/main/java/com/github/aureliano/verbum_domini/web/VerbumDominiApiResourceConfig.java
+++ src/main/java/com/github/aureliano/verbum_domini/web/VerbumDominiApiResourceConfig.java
@@ -2,22 +2,22 @@ package com.github.aureliano.verbum_domini.web;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
+import com.github.aureliano.verbum_domini.resource.bible.AnnotationsResource;
 import com.github.aureliano.verbum_domini.resource.bible.BiblesResource;
 import com.github.aureliano.verbum_domini.resource.bible.BooksResource;
 import com.github.aureliano.verbum_domini.resource.bible.ChaptersResource;
 import com.github.aureliano.verbum_domini.resource.bible.VersesResource;
-import com.github.aureliano.verbum_domini.resource.impl.AnnotationsResourceImpl;
 
 public class VerbumDominiApiResourceConfig extends ResourceConfig {
 
 	public VerbumDominiApiResourceConfig() {
-		packages("com.github.aureliano.verbum_domini.resource.impl");
+		packages("com.github.aureliano.verbum_domini.resource.bible");
 		
 		register(CharsetResponseFilter.class);
 		register(BiblesResource.class);
 		register(BooksResource.class);
 		register(ChaptersResource.class);
 		register(VersesResource.class);
-		register(AnnotationsResourceImpl.class);
+		register(AnnotationsResource.class);
 	}
 }
