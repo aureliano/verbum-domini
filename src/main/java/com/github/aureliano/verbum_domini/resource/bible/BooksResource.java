67b2f4fba5674a24d7dec5c498e3788ec65aa113
aureliano <aureliano.franca@hotmail.com>
2015-09-13 21:56:25 -0300

    Fetch book by id service.

--- src/main/java/com/github/aureliano/verbum_domini/resource/bible/BooksResource.java
+++ src/main/java/com/github/aureliano/verbum_domini/resource/bible/BooksResource.java
@@ -2,11 +2,13 @@ package com.github.aureliano.verbum_domini.resource.bible;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import com.github.aureliano.verbum_domini.model.Book;
 import com.github.aureliano.verbum_domini.model.Books;
 import com.github.aureliano.verbum_domini.service.BooksService;
 
@@ -25,5 +27,20 @@ public class BooksResource {
 		
 		Books books = BooksService.fetchAll(start, pages);
 		return Response.status(200).entity(books).build();
+	}
+
+	@Path("{bookId}")
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public Response getBookById(
+			@PathParam("bookId") String bookId) {
+		
+		Book book = BooksService.fetchBookById(bookId);
+		
+		if (book == null) {
+			return Response.status(404).build();
+		}
+		
+		return Response.status(200).entity(book).build();
 	}
 }
--- src/main/java/com/github/aureliano/verbum_domini/web/VerbumDominiApiResourceConfig.java
+++ src/main/java/com/github/aureliano/verbum_domini/web/VerbumDominiApiResourceConfig.java
@@ -3,8 +3,8 @@ package com.github.aureliano.verbum_domini.web;
 import org.glassfish.jersey.server.ResourceConfig;
 
 import com.github.aureliano.verbum_domini.resource.bible.BiblesResource;
+import com.github.aureliano.verbum_domini.resource.bible.BooksResource;
 import com.github.aureliano.verbum_domini.resource.impl.AnnotationsResourceImpl;
-import com.github.aureliano.verbum_domini.resource.impl.BooksResourceImpl;
 import com.github.aureliano.verbum_domini.resource.impl.ChaptersResourceImpl;
 import com.github.aureliano.verbum_domini.resource.impl.VersesResourceImpl;
 
@@ -15,7 +15,7 @@ public class VerbumDominiApiResourceConfig extends ResourceConfig {
 		
 		register(CharsetResponseFilter.class);
 		register(BiblesResource.class);
-		register(BooksResourceImpl.class);
+		register(BooksResource.class);
 		register(ChaptersResourceImpl.class);
 		register(VersesResourceImpl.class);
 		register(AnnotationsResourceImpl.class);
