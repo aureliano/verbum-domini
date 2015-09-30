7ee21eb21869af47d1615e5df0b7ba6bb0278433
aureliano <aureliano.franca@hotmail.com>
2015-09-29 23:42:14 -0300

    Sign in implementation.

--- src/main/java/com/github/aureliano/verbum_domini/web/bc/SignInBC.java
+++ src/main/java/com/github/aureliano/verbum_domini/web/bc/SignInBC.java
@@ -5,6 +5,8 @@ import java.util.List;
 
 import javax.faces.application.FacesMessage;
 
+import com.github.aureliano.verbum_domini.core.impl.dao.UserDaoImpl;
+
 public final class SignInBC {
 
 	private SignInBC() {
@@ -13,6 +15,17 @@ public final class SignInBC {
 	
 	public static List<FacesMessage> authenticate(String login, String password) {
 		List<FacesMessage> messages = new ArrayList<>();
+		
+		try {
+			boolean authenticated = new UserDaoImpl().authenticate(login, password);
+
+			if (!authenticated) {
+				String message = "User does not exist or password is wrong.";
+				messages.add(new FacesMessage(FacesMessage.SEVERITY_ERROR, message, message));
+			}
+		} catch (Exception ex) {
+			messages.add(new FacesMessage(FacesMessage.SEVERITY_ERROR, ex.getMessage(), ex.getMessage()));
+		}
 		
 		return messages;
 	}
