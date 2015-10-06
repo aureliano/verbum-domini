1e643958aa40247978a7aab10de36d640fe9b406
aureliano <aureliano.franca@hotmail.com>
2015-10-06 19:36:52 -0300

    Bible edition implementation.

--- src/main/java/com/github/aureliano/verbum_domini/web/mb/BibleEditMB.java
+++ src/main/java/com/github/aureliano/verbum_domini/web/mb/BibleEditMB.java
@@ -1,36 +1,41 @@
 package com.github.aureliano.verbum_domini.web.mb;
 
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.view.ViewScoped;
+
+import org.apache.log4j.Logger;
 
 import com.github.aureliano.verbum_domini.core.bean.BibleBean;
 import com.github.aureliano.verbum_domini.core.impl.bean.BibleBeanImpl;
 import com.github.aureliano.verbum_domini.helper.WebHelper;
 import com.github.aureliano.verbum_domini.web.bc.BiblesBC;
 
-@ManagedBean
+@ManagedBean(name = "bibleEditMB")
 @ViewScoped
 public class BibleEditMB {
 
+	private static final Logger logger = Logger.getLogger(BibleEditMB.class);
+	
 	private BibleBean bible;
 	
 	public BibleEditMB() {
 		super();
 	}
 	
+	@PostConstruct
 	public void preRender() {
-		System.out.println("\n\n ==>> Executing preRender");
 		Integer id = this.bibleId();
+		logger.info("Preparing to edit bible with id " + id);
 		this.bible = (id != null) ? BiblesBC.fetchBible(id) : new BibleBeanImpl();
-		System.out.println(" ==>> BIBLE: " + this.bible + "\n\n");
 	}
 	
 	public void save() {
-		System.out.println("\n\n => Calling save");
+		logger.info("Saving bible with id " + this.bible.getId());
 	}
 
 	public BibleBean getBible() {
-		return bible;
+		return this.bible;
 	}
 
 	public void setBible(BibleBean bible) {
