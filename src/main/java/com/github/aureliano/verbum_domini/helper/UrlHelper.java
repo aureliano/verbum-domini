922615850dc310f94a1b186a042498a7dd245990
aureliano <aureliano.franca@hotmail.com>
2015-10-15 23:18:01 -0300

    Add method to build URLs with context path.

--- src/main/java/com/github/aureliano/verbum_domini/helper/UrlHelper.java
+++ src/main/java/com/github/aureliano/verbum_domini/helper/UrlHelper.java
@@ -1,5 +1,8 @@
 package com.github.aureliano.verbum_domini.helper;
 
+import javax.servlet.ServletRequest;
+import javax.servlet.http.HttpServletRequest;
+
 import org.apache.commons.lang.StringUtils;
 
 public final class UrlHelper {
@@ -10,5 +13,14 @@ public final class UrlHelper {
 	
 	public static String join(Object...tokens) {
 		return "/" + StringUtils.join(tokens, '/');
+	}
+	
+	public static String buildWebAppUrl(ServletRequest request, String path) {
+		String contextPath = ((HttpServletRequest) request).getContextPath();
+		return join(contextPath.substring(1), path);
+	}
+	
+	public static String buildWebAppUrl(String path) {
+		return buildWebAppUrl(WebHelper.getRequest(), path);
 	}
 }
