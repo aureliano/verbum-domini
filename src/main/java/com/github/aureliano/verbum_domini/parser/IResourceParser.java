bec7f15c887106881880deb12efe8d70759d7f89
aureliano <aureliano.franca@hotmail.com>
2015-10-21 16:27:13 -0200

    Configure main project.

--- src/main/java/com/github/aureliano/verbum_domini/parser/IResourceParser.java
+++
@@ -1,8 +0,0 @@
-package com.github.aureliano.verbum_domini.parser;
-
-import com.github.aureliano.verbum_domini.core.bean.IBean;
-
-public interface IResourceParser<T extends IBean> {
-
-	public abstract Object toResource(T bean);
-}
